eca0c81467a707fbaa4b69a4d63e6ce228b1e8c8
DhirajGadekar <dhirajgadekar629@gmail.com>
2025-08-16 14:35:38 +0530

    feature: add constant class for controller utilities

---
+++ src/main/java/com/meal_bridge/constants/ControllerConstant.java
@@ -0,0 +1,7 @@
+package com.meal_bridge.constants;
+
+public class ControllerConstant {
+
+    public final static String LOGIN = "/login";
+    public final static String REGISTER = "/register";
+}
